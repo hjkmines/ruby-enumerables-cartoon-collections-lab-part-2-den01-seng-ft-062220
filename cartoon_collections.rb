33a1225368be5a0081e3a3d00486a030035ef9c5
Hyung Kim <hjkmines@gmail.com>
2020-06-17 22:32:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,19 @@
 def square_array(array) 
-  
-  
+  new_array = []
+  array.each do |x|
+    new_array.push(x ** 2) 
   
   
   
 end
 
-
+def capitalize(array)
+  
+  array.map do |x|
+    x.capitalize + "!"
+  end 
+  
+end
 
 
 
